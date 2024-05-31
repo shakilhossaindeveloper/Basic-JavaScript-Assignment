004785d2ce809d6a176f9e595c7e1550964cdc93
shakilhossaindeveloper <shakilhossain.developer@gmail.com>
2024-05-31 16:15:44 +0600

    javabasicassignment

---
+++ javaassignment.js
@@ -0,0 +1,116 @@
+
+// Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo";
+
+//On one single line, declare three variables with the following names and values:
+
+
+let firstName = "John";     //1st variable name is firstName
+let lastName = "Doe";    //variable value  is Doe this is string data type
+let age = 35;              //variable name is age And Value 35
+
+
+
+// Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+// 1st way
+let x = 10 * 5;    // x er value 10 ar y er value 5
+// console.log(x)    x = 50 will be the value of x 
+
+// 2nd
+let x = 10;
+let y = 5;
+x*= y
+// console.log(x)     x = 50 will be the value of x 
+
+
+
+// Use comments to describe the correct data type of the following variables:
+
+let length = 16; // number data type 
+let lastName = "Johnson"; // string data type  
+
+ const x = {
+   firstName: "John",  
+   lastName: "Doe"
+};     //object data type  
+
+
+
+// Execute the function named myFunction.
+ 
+function myFunction() {
+    alert("Hello World!");
+  }
+  
+  // call the function and see the output 
+  myFunction();
+
+
+//   1. Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50").
+
+let person = {
+    name: "John",
+    age: 50,
+}
+ alert(person.name, age)
+
+
+
+
+//  1. The <button> element should do something when someone clicks on it. Try to fix it!
+
+<button >Clickme.</button>  //akhane problem ki bujlam na 
+
+   <button >Click Me</button>
+
+//    1. Alert the number of items in an array, using the correct Array property: 
+
+    const cars = ["Volvo", "Jeep", "Mercedes"];
+    alert(cars.length)
+
+
+
+// 2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+    Brand[0] = "Ford"
+
+
+
+
+
+
+    // 1. Use the correct Math method to create a random number.
+    let ran = Math.random()
+    // console.log(ran) output er jonno 
+
+    
+
+
+    // 2. Use the correct Math method to return the largest number of 10 and 20.
+    let maxi = Math.max(10, 20)
+    // console.log(maxi) output er jonno 
+
+    
+    // 3. Use the correct Math method to get the square root of 9.
+    let sroot = Math.sqrt(9)
+    // console.log(sroot)  output dekhar jonno    
+
+    // 1. Choose the correct comparison operator to alert true, when x is greater than y. 
+    let x= 10;
+    let y = 5;
+    // alert( x > y) //  output true
+    
+    
+    
+    // 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+    let age = 17
+    alert(age < 18 ? "Too young" : "Old enough");
+    
+    
+ 
